598c1da26f8b13a38d23e5e0b52bc7f3ef53c8a9
Lminta <shakurov.ve@gmail.com>
2021-04-06 22:31:22 +0300

    Проект добавлен на гитхаб

---
+++ Assets/ITS/UI/Popup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ITS.UI
+{
+    public class Popup : MonoBehaviour
+    {
+        [SerializeField]
+        TMP_Text _header;
+        [SerializeField]
+        TMP_Text _message;
+
+        [SerializeField]
+        Button _rightButton;
+        [SerializeField]
+        Button _leftButton;
+        [SerializeField]
+        TMP_Text _right;
+        [SerializeField]
+        TMP_Text _left;
+
+        public void SetButtonsCallback(Action onRightClicked, Action onLeftClicked)
+        {
+            _rightButton.onClick.AddListener(() =>
+            {
+                onRightClicked?.Invoke();
+            });
+
+            _leftButton.onClick.AddListener(() =>
+            {
+                onLeftClicked?.Invoke();
+            });
+        }
+
+        public void SetText(string header, string message, string right, string left)
+        {
+            _header.text = header;
+            _message.text = message;
+            _right.text = right;
+            _left.text = left;
+        }
+    }
+}
---
+++ Assets/ITS/UI/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace ITS.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform _canvas;
+
+        [SerializeField]
+        Popup _popupPrefab;
+
+        public void ShowPopup(string header, string message, string right, string left, Action rightAnswer, Action leftAnswer)
+        {
+            Popup dialogue = Instantiate(_popupPrefab, _canvas);
+            dialogue.SetText(header, message, right, left);
+            dialogue.SetButtonsCallback(rightAnswer, leftAnswer);
+        }
+    }
+}
